cf155cd655f404a6312fe3c74532c4ce180280a4
DiegoMerida28 <145516421+DiegoMerida28@users.noreply.github.com>
2023-09-30 21:16:39 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplos de arreglos-c#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 10, 20, 30, 40, 50, 60 };
+        Console.WriteLne(+numeros[3]);
+
+        int n = new int { 3, 4, 5 };
+
+        char[] c = { 'L', 'u', 'i', 's' };
+
+        string s = "Mona Lisa";
+
+
+        Console.ReadKey();
+    }
+}
